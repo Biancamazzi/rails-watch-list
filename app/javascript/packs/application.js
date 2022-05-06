fbe3816678966a46e302ac7245e8077ec680e5a6
BiancaMazzi <bianca.mazzi@hotmail.com>
2022-05-05 22:09:53 -0300

    update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+// Stimulus
+const application = Application.start()
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
+import "controllers"
 
 Rails.start()
 Turbolinks.start()
